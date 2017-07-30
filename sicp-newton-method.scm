2dd774eb4f9fbadcaf2aa259c57663db128fc647
Xincheng Zhang <xinchengzhang@Xinchengs-MacBook-Pro.local>
2017-07-30 11:11:39 +0800

    ddd

---
+++ sicp-newton-method.scm
@@ -0,0 +1,12 @@
+((define (square x)
+  (* x x)))
+((define (average a b)
+  (/ (+ a b) 2)))
+((define (improve guess x)
+  (average guess (/ x guess))))
+((define (good-enough? guess x)
+  (< (abs (- (square guess) x))) 0.001))
+((define (sqrt-iter guess x)
+  ((if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))))
